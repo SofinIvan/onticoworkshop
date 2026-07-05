9ea996efd5325885d3cd2207bffb52221fa38bfc
isofin <i.sofin@gmail.com>
2026-07-05 16:55:51 +0300

    fix: add unique constraints on User username and email

--- backend/src/main/java/com/onticoworkshop/model/User.java
+++ backend/src/main/java/com/onticoworkshop/model/User.java
@@ -1,5 +1,6 @@
 package com.onticoworkshop.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -17,10 +18,12 @@ public class User {
   @Id
   private String id;
 
+  @Column(unique = true)
   private String username;
 
   private String displayName;
 
+  @Column(unique = true)
   private String email;
 
   private String timezone;
